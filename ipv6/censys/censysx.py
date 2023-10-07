603f317462066466e426fdae6d6e5fb07cd0ae49
Mateus <77507023+r1beirin@users.noreply.github.com>
2023-10-06 23:25:09 -0300

    Update censysx.py

--- ipv6/censys/censysx.py
+++ ipv6/censys/censysx.py
@@ -10,14 +10,9 @@ json_list = []
 today = date.today()
 output_filename = f'censys-{today}.json'
 
-# asia-east2-instance-02 		2600:1900:41a0:138:0:0:0:0
-# europe-west3-instance-02 	2600:1900:40d0:7bc2:0:0:0:0
-# me-west1-instance-02 		2600:1901:8160:d0:0:0:0:0
-# southamerica-east1-instance-02	2600:1900:40f0:f9f2:0:0:0:0
-# us-west2-instance-02 		2600:1900:4120:ecf8:0:0:0:0
-instancesIP = ['2600:1900:41a0:138:0:0:0:0', '2600:1900:40d0:7bc2:0:0:0:0', '2600:1901:8160:d0:0:0:0:0', '2600:1900:40f0:f9f2:0:0:0:0', '2600:1900:4120:ecf8:0:0:0:0']
+instancesIP = ['2600:1900:41a0:9b2f:0:0:0:0', '2600:1900:40d0:e44c:0:0:0:0', '2600:1901:8160:716:0:0:0:0', '2600:1900:40f0:fce8:0:0:0:0', '2600:1900:4120:d82b:0:0:0:0']
 
-for ip in instancesIP():
+for ip in instancesIP:
       result = api.view(ip)
       json_list.append(result)
       print(result)
